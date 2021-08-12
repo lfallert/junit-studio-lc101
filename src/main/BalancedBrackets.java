74295a73fbb07af58aad3b6f8df49872eeb08595
LFallert <lfallert13@yahoo.com>
2021-08-11 21:25:41 -0500

    Unit testing studio

--- src/main/BalancedBrackets.java
+++ src/main/BalancedBrackets.java
@@ -5,18 +5,18 @@ public class BalancedBrackets {
     /**
      * The function BalancedBrackets should return true if and only if
      * the input string has a set of "balanced" brackets.
-     *
+     * <p>
      * That is, whether it consists entirely of pairs of opening/closing
      * brackets (in that order), none of which mis-nest. We consider a bracket
      * to be square-brackets: [ or ].
-     *
+     * <p>
      * The string may contain non-bracket characters as well.
-     *
+     * <p>
      * These strings have balanced brackets:
-     *  "[LaunchCode]", "Launch[Code]", "[]LaunchCode", "", "[]"
-     *
+     * "[LaunchCode]", "Launch[Code]", "[]LaunchCode", "", "[]"
+     * <p>
      * While these do not:
-     *   "[LaunchCode", "Launch]Code[", "[", "]["
+     * "[LaunchCode", "Launch]Code[", "[", "]["
      *
      * @param str - to be validated
      * @return true if balanced, false otherwise
@@ -28,8 +28,19 @@ public class BalancedBrackets {
                 brackets++;
             } else if (ch == ']') {
                 brackets--;
+                if (str.indexOf("[") < str.indexOf("]")) {
+                    return true;
+                } else {
+                    return false;
+                }
+            } else if (!str.contains("[") && !str.contains("]")) {
+                return false;
+            }
+            else if (
+            str.chars().filter(num -> num == '[').count() != str.chars().filter(num -> num == ']').count()) {
+                return false;
             }
         }
-        return brackets == 0;
+            return brackets == 0;
     }
 }
--- src/test/BalancedBracketsTest.java
+++ src/test/BalancedBracketsTest.java
@@ -1,5 +1,7 @@
 package test;
 
+import main.BalancedBrackets;
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -10,6 +12,72 @@ public class BalancedBracketsTest {
     @Test
     public void emptyTest() {
         assertEquals(true, true);
+    }
+
+    @Test
+    public void onlyBracketsReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[]"));
+    }
+
+    @Test
+    public void reverseBracketsReturnsFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("]["));
+    }
+
+    @Test
+    public void returnsTrueIfStringWithBrackets() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[LaunchCode]"));
+    }
+
+    @Test
+    public void returnsFalseIfStringHasNoBrackets() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("LaunchCode"));
+    }
+
+    @Test
+    public void returnsFalseIfMultipleBrackets() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("[[LaunchCode]"));
+    }
+
+    //I am so sorry for this test name, I was tired
+    @Test
+    public void returnsFalseIfMultipleBracketsButInOpposite() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("[LaunchCode]]"));
+    }
+
+    @Test
+    public void returnsTrueIfBracketsInWord() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("Launch[C]ode"));
+    }
+
+    @Test
+    public void returnsFalseIfReverseBracketsInString() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("]LaunchCode["));
+    }
+
+    @Test
+    public void returnsTrueIfDoubleBrackets() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[[LaunchCode]]"));
+    }
+
+    @Test
+    public void returnsTrueWithSpecialCharacters(){
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[%%%]"));
+    }
+
+    @Test
+    public void returnsTrueIfEmpty() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets(""));
+    }
+
+    @Test
+    public void returnsFalseIfOneBracket() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("["));
+    }
+
+    @Test
+    public void returnsTrueIfAllTheBrackets() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[[[[]]]]"));
     }
 
 
